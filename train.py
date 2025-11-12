b38fc63b6f345050edf8c82780a7ffb0e2dbcaf8
hatzulURL <hatzul@correo.url.edu.gt>
2025-11-11 20:06:37 -0600

    Update train.py
    Actualización metodo de entrenamiento del modelo

--- train.py
+++ train.py
@@ -1,12 +1,32 @@
-from sklearn.datasets import load_iris
+from sklearn.datasets import load_wine
 from sklearn.linear_model import LogisticRegression
 import pickle
+import pandas as pd
 
-print("Entrenando modelo...")
-X, y = load_iris(return_X_y=True)
-clf = LogisticRegression(random_state=0, max_iter=1000).fit(X, y)
+print("Cargando dataset de Vinos...")
+wine = load_wine(as_frame=True)
+X = wine.data
+y = wine.target
 
-with open('modelo.pkl', 'wb') as f:
+# De las 13 características, seleccionamos las 4 más importantes.
+features_importantes = [
+    'proline', 
+    'flavanoids', 
+    'color_intensity', 
+    'alcohol'
+]
+
+X_subset = X[features_importantes]
+
+print(f"Entrenando modelo usando solo estas 4 características: {features_importantes}")
+
+# Entrenar el modelo solo con el subconjunto de características
+clf = LogisticRegression(random_state=0, max_iter=2000).fit(X_subset, y)
+
+# Guardar el nuevo modelo
+with open('modelo_wine.pkl', 'wb') as f:
     pickle.dump(clf, f)
 
-print("¡Modelo guardado en modelo.pkl!")
+print("¡Modelo de Vinos guardado en modelo_wine.pkl!")
+print("\nValores de ejemplo para los sliders:")
+print(X_subset.describe())
